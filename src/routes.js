2b70a5a34fd6435b824fb6432fc197f199f93767
gcairesdev <pessoalgcaires@gmail.com>
2020-06-13 15:23:35 -0300

    :rocket: Added tab navigation

--- src/routes.js
+++ src/routes.js
@@ -3,20 +3,39 @@ import 'react-native-gesture-handler';
 import * as React from 'react';
 
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 import Home from './pages/Home';
 import Detail from './pages/Detail';
 
-const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
 const Routes = () => {
 	return(
     <NavigationContainer>
-      <Stack.Navigator initalRouteName="Home">
-        <Stack.Screen name="Home" component={Home} options={{headerShown:false}} />
-        <Stack.Screen name="Detail" component={Detail} options={{headerShown:false}} />
-      </Stack.Navigator>
+      <Tab.Navigator
+      	screenOptions={({ route }) => ({
+				  tabBarIcon: ({ focused, color, size }) => {
+				  	let iconName;
+
+				    if (route.name === 'World') {
+				      iconName = 'earth';
+				    } else if (route.name === 'Virus') {
+				      iconName = 'bacteria-outline';
+				    }
+
+				    return <MaterialCommunityIcons name={iconName} size={size} color={color} />;
+	  			},
+				})}
+				tabBarOptions={{
+				  activeTintColor: '#d63226',
+				  inactiveTintColor: 'gray',
+				}}
+      >
+        <Tab.Screen name="World" component={Home} />
+        <Tab.Screen name="Virus" component={Detail} />
+      </Tab.Navigator>
     </NavigationContainer>
 	);
 };
